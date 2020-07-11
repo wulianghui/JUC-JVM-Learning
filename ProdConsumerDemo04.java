c6e222944b93edd90f19bce1e7d07e0ae479d7dc
wlh <wliangh@163.com>
2020-07-11 12:04:23 +0800

    juc_third day

--- ProdConsumerDemo04.java
+++ ProdConsumerDemo04.java
@@ -8,7 +8,7 @@ class Aircondition{
     public synchronized void increment() throws Exception{
 
         //1 判断
-        if(number != 0){
+        while (number != 0){
             this.wait();
         }
         //2 干活
@@ -20,7 +20,15 @@ class Aircondition{
     }
 
     public synchronized void decrement() throws Exception{
+
+        while (number == 0){
+            this.wait();
+        }
+
         number --;
+        System.out.println(Thread.currentThread().getName() + "\t" + number);
+
+        this.notifyAll();
     }
 }
 /**
@@ -31,8 +39,58 @@ class Aircondition{
  *
  * 1    高聚低合前提下，线程操作资源类
  * 2    判断/干活/通知
+ * 3    todo 防止虚假唤醒
  */
 public class ProdConsumerDemo04 {
 
+    public static void main(String[] args) {
+
+        Aircondition aircondition =new Aircondition();
+
+        new Thread(()->{
+
+            for (int i = 1; i<=10; i++){
+                try {
+                    Thread.sleep(200);
+                    aircondition.increment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"A").start();
+
+        new Thread(()->{
+            for (int i = 1; i<=10; i++){
+                try {
+                    Thread.sleep(300);
+                    aircondition.decrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"B").start();
+
+        new Thread(()->{
+            for (int i = 1; i<=10; i++){
+                try {
+                    Thread.sleep(400);
+                    aircondition.increment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"C").start();
+
+        new Thread(()->{
+            for (int i = 1; i<=10; i++){
+                try {
+                    Thread.sleep(500);
+                    aircondition.decrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"D").start();
+    }
 
 }
